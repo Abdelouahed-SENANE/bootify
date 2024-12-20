22ca07c12097f6d96993f971cee359ed5d03db16
Abdelouahed-SENANE <abdosenane75@gmail.com>
2024-12-20 19:38:28 +0100

    intializer boottify project

---
+++ src/main/java/ma/youcode/bootify/BootifyApplication.java
@@ -0,0 +1,13 @@
+package ma.youcode.bootify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/youcode/bootify/BootifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youcode.bootify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
